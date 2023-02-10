e861a303ed6b0b8efac332ccdfccc2794c3857bb
penborter <benpo@outlook.com>
2023-02-10 17:14:42 +1100

    Update

--- leagueleaders.py
+++ leagueleaders.py
@@ -6,17 +6,20 @@ from nba_api.stats.library.parameters import Season
 
 def get_data(cat, per_mode):
 
-  leagueLead = leagueleaders.LeagueLeaders(league_id='00',
-                                         per_mode48=per_mode,
-                                         scope='S',
-                                         season=Season.default,
-                                         season_type_all_star='Regular Season',
-                                         stat_category_abbreviation=cat)
-  
-  # Export just the top 30 to csv
-  leaders = leagueLead.league_leaders.get_data_frame()
-  csv_name = 'data/dynamic/NBA_Leaders_' + cat + per_mode + '.csv'
-  leaders.iloc[:30].to_csv(csv_name,index=False)
+  try:
+    leagueLead = leagueleaders.LeagueLeaders(league_id='00',
+                                          per_mode48=per_mode,
+                                          scope='S',
+                                          season=Season.default,
+                                          season_type_all_star='Regular Season',
+                                          stat_category_abbreviation=cat)
+    
+    # Export just the top 30 to csv
+    leaders = leagueLead.league_leaders.get_data_frame()
+    csv_name = 'data/dynamic/NBA_Leaders_' + cat + per_mode + '.csv'
+    leaders.iloc[:30].to_csv(csv_name,index=False)
+  except:
+    return
 
 
 # Extract and export the info for each of the listed categories
