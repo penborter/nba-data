16ef3e338a3831db2f2c08225e54fe4bf5a84617
penborter <benpo@outlook.com>
2024-03-01 16:38:27 +1100

    Test Moreyball action

--- moreyball.py
+++ moreyball.py
@@ -1,61 +1,71 @@
-import numpy as np
+import time
 import pandas as pd
+import numpy as np
 
 from nba_api.stats.endpoints import leaguedashplayershotlocations
 
+# Retry Wrapper 
+def retry(func, retries=3):
+    def retry_wrapper(*args, **kwargs):
+        attempts = 0
+        while attempts < retries:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                print(e)
+                time.sleep(30)
+                attempts += 1
 
-## Extracted from nba_api - for some reason doesn't work as in-built class method
-def get_data_frame(data):
+    return retry_wrapper
 
-    if isinstance(data["headers"][0], str):
-        return DataFrame(data["data"], columns=data["headers"])
 
-    else:  # Multiple levels of column names
-        levels = []
-        level_names = []
-        for i in range(
-            len(data["headers"])
-        ):  # Extend column names for level to full length
-            level = data["headers"][i]
-            level_names.append(
-                level["name"] if "name" in level else "LEVEL_" + str(i)
-            )
-            column_names = (
-                [""] * level["columnsToSkip"]
-                if "columnsToSkip" in level
-                else []
-            )
-            column_names += list(
-                np.repeat(
-                    np.array(level["columnNames"]),
-                    level["columnSpan"] if "columnSpan" in level else 1,
-                )
-            )
-            levels.append(column_names)
-        midx = pd.MultiIndex.from_arrays(
-            levels, names=level_names
-        )  # Use MultiIndex for dataframe columns
-        return pd.DataFrame(data["data"], columns=midx)
+def getShootingDF():
+  
+  @retry
+  def getShootingData():
+    data = leaguedashplayershotlocations.LeagueDashPlayerShotLocations()
+    data = data.shot_locations.data
+
+    # This section from nba_api package - couldn't get the built-in class method to work
+    levels = []
+    level_names = []
+    for i in range(len(data["headers"])):  # Extend column names for level to full length
+      level = data["headers"][i]
+      level_names.append(
+        level["name"] if "name" in level else "LEVEL_" + str(i)
+      )
+      column_names = (
+        [""] * level["columnsToSkip"]
+        if "columnsToSkip" in level
+        else []
+      )
+      column_names += list(
+        np.repeat(
+          np.array(level["columnNames"]),
+          level["columnSpan"] if "columnSpan" in level else 1,
+        )
+      )
+      levels.append(column_names)
+    midx = pd.MultiIndex.from_arrays(
+      levels, names=level_names
+    )  # Use MultiIndex for dataframe columns
     
+    return pd.DataFrame(data["data"], columns=midx)
 
-try:
-  playerShotlocs = leaguedashplayershotlocations.LeagueDashPlayerShotLocations()
-  playershotsDF = get_data_frame(playerShotlocs.shot_locations.data)
 
-except Exception as err:
-    print(f'Error: {err}')
-
-finally:
-  # Add new columns to calculate Moreyball percentage
+  shootingDF = getShootingData()
   for fg in ['FGM', 'FGA']:
-      playershotsDF.loc[:, ('Total Shots', fg)] = playershotsDF.xs(fg, level=1, axis=1).sum(axis=1) - playershotsDF['Corner 3'][fg]
-      playershotsDF.loc[:, ('Total from 3', fg)] = playershotsDF[['Above the Break 3', 'Corner 3', 'Backcourt']].xs(fg, level=1, axis=1).sum(axis=1)
-      playershotsDF.loc[:, ('Pct RA', fg)] = round(playershotsDF['Restricted Area'][fg] / playershotsDF['Total Shots'][fg], 3)
-      playershotsDF.loc[:, ('Pct 3', fg)] = round(playershotsDF['Total from 3'][fg] / playershotsDF['Total Shots'][fg], 3)
-      playershotsDF.loc[:, ('Pct Moreyball', fg)] = round(playershotsDF['Pct RA'][fg] + playershotsDF['Pct 3'][fg], 3)
+    shootingDF.loc[:, ('Total Shots', fg)] = shootingDF.xs(fg, level=1, axis=1).sum(axis=1) - shootingDF['Corner 3'][fg]
+    shootingDF.loc[:, ('Total from 3', fg)] = shootingDF[['Above the Break 3', 'Corner 3', 'Backcourt']].xs(fg, level=1, axis=1).sum(axis=1)
+    shootingDF.loc[:, ('Pct RA', fg)] = round(shootingDF['Restricted Area'][fg] / shootingDF['Total Shots'][fg], 3)
+    shootingDF.loc[:, ('Pct 3', fg)] = round(shootingDF['Total from 3'][fg] / shootingDF['Total Shots'][fg], 3)
+    shootingDF.loc[:, ('Pct Moreyball', fg)] = round(shootingDF['Pct RA'][fg] + shootingDF['Pct 3'][fg], 3)
+
+  shootingDF = shootingDF[shootingDF['Total Shots']['FGA'] > 200].sort_values(by=('Pct Moreyball', 'FGA'), ascending=False)
+  shootingDF.columns = [col[1] if col[0] == "" else '_'.join(col) for col in shootingDF.columns.values]
+
+  return shootingDF
 
 
-  sortedDF = playershotsDF[playershotsDF['Total Shots']['FGA'] > 200].sort_values(by=('Pct Moreyball', 'FGA'), ascending=False)
-  sortedDF.columns = [col[1] if col[0] == "" else '_'.join(col) for col in sortedDF.columns.values]
-
-  sortedDF.to_csv('data/dynamic/Moreyball_Ranking.csv', index=False)
+shootingDF = getShootingDF()
+shootingDF.to_csv('data/dynamic/Moreyball_Ranking.csv', index=False)
