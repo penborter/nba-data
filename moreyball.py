0523b5ae439ccbc3706016fa045f20a27c1f13c1
penborter <benpo@outlook.com>
2024-10-26 12:35:08 +1100

    Update action

--- moreyball.py
+++ moreyball.py
@@ -28,7 +28,7 @@ def get_shooting_data():
   data = leaguedashplayershotlocations.LeagueDashPlayerShotLocations()
   return data.shot_locations.get_data_frame()
 
-def process_shooting_data(df):
+def process_shooting_data(shootingDF):
   for fg in ['FGM', 'FGA']:
     shootingDF.loc[:, ('Total Shots', fg)] = shootingDF.xs(fg, level=1, axis=1).sum(axis=1) - shootingDF['Corner 3'][fg]
     shootingDF.loc[:, ('Total from 3', fg)] = shootingDF[['Above the Break 3', 'Corner 3', 'Backcourt']].xs(fg, level=1, axis=1).sum(axis=1)
@@ -36,7 +36,7 @@ def process_shooting_data(df):
     shootingDF.loc[:, ('Pct 3', fg)] = round(shootingDF['Total from 3'][fg] / shootingDF['Total Shots'][fg], 3)
     shootingDF.loc[:, ('Pct Moreyball', fg)] = round(shootingDF['Pct RA'][fg] + shootingDF['Pct 3'][fg], 3)
 
-  shootingDF = shootingDF[shootingDF['Total Shots']['FGA'] > 200].sort_values(by=('Pct Moreyball', 'FGA'), ascending=False)
+  shootingDF = shootingDF.sort_values(by=('Pct Moreyball', 'FGA'), ascending=False)
   shootingDF.columns = [col[1] if col[0] == "" else '_'.join(col) for col in shootingDF.columns.values]
 
   return shootingDF
--- resources.md
+++ resources.md
@@ -5,10 +5,10 @@ Quick links and resources for reference. Work in progress.
 
 - [PBP Stats](https://www.pbpstats.com/)
 - [Basketball-Reference.com](https://www.basketball-reference.com/)
+- [Pro Sports Transactions - Basketball](https://www.prosportstransactions.com/basketball/)
 
 ## Predictions
 
 - [NBA Playoff Picture](http://www.playoffstatus.com/nba/nba.html)
-- [2022-23 NBA Predictions | FiveThirtyEight](https://projects.fivethirtyeight.com/2023-nba-predictions/)
 - [Tankathon | NBA Draft Order & Lottery Simulator](https://www.tankathon.com/)
 
